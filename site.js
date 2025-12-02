ea8fa1003233f4db429eb3f483d597437a3b1213
526cloiacono <526cloiacono@frhsd.com>
2025-12-02 15:25:12 +0000

    three

--- site.js
+++ site.js
@@ -20,26 +20,25 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
 
-
 const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
-      created () {
+      created() {
             fetch('movies.json').then(response => response.json()).then(json => {
                   this.movies = json
             })
       },
       data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-         
-      }
-    },
+       
+            return {
+                  movies: [],
+                  title: "",
+                  owner: "Christopher Loiacono",
+                  github: "https://github.com/526cloiacono",
+                  /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+
+            }
+      },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
       }
-})
-
-vue_app.mount("#vue_app")
