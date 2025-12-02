6da9f80d0e08fdf108576aa35c33b8510f233f1a
526cloiacono <526cloiacono@frhsd.com>
2025-12-02 15:42:34 +0000

    seven

--- site.js
+++ site.js
@@ -20,6 +20,8 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
 
+
+
 const vue_app = Vue.createApp({
       // This automatically imports your movies.json file and puts it into
       //   the variable: movies
@@ -29,16 +31,43 @@ const vue_app = Vue.createApp({
             })
       },
       data() {
-       
             return {
+                  // This holds your movies.json data.
                   movies: [],
                   title: "",
                   owner: "Christopher Loiacono",
                   github: "https://github.com/526cloiacono",
-                  /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            
 
             }
       },
       methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+      
+            makeTextDate(dateArray) {
+                  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+                  let year = dateArray[0];
+                  let month = months[dateArray[1] - 1];
+                  let day = dateArray[2];
+                  return month + " " + day + ", " + year;
+
+            },
+            like(index) {
+                  this.movies[index].likes++
+            },
+            dislike(index) {
+                  this.movies[index].dislikes++
+
+            },
+            posterClick(index) {
+                  let movie = this.movies[index];
+                  movie.posterindex = (movie.posterindex + 1) % movie.posters.length;
+            },
+            timeText(minutes) {
+                  let hrs = Math.floor(minutes / 60);
+                  let mins = minutes % 60;
+                  return hrs + "h " + mins + "min";
+            },
       }
+})
+
+vue_app.mount("#vue_app")
