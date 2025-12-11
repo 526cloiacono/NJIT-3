f83225abe979e1fad0d47f69b6aed799ea31da7c
526cloiacono <526cloiacono@frhsd.com>
2025-12-11 02:13:12 +0000

    added carosell

--- site.js
+++ site.js
@@ -1,49 +1,52 @@
-
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      //   the variable: movies
-      created() {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
-      },
-       data() {
-            return {
-                  // This holds your movies.json data.
-                  movies: [],
-                  title: "",
-                  owner: "Christopher Loiacono",
-                  github: "https://github.com/526cloiacono",
-            
+  created() {
+    // Fetch movies.json when page loads
+    fetch('movies.json')
+      .then(response => response.json())
+      .then(json => { this.movies = json });
+  },
+  data() {
+    return {
+      movies: [],
+      currentIndex: 0,           // Track which movie is currently shown
+      title: "My Movie Carousel",
+      owner: "Christopher Loiacono",
+      github: "https://github.com/526cloiacono",
+    };
+  },
+  computed: {
+    // Return the currently active movie
+    currentMovie() {
+      return this.movies[this.currentIndex];
+    }
+  },
+  methods: {
+    // Convert date array to human-readable string
+    makeTextDate(dateArray) {
+      const months = ["January","February","March","April","May","June",
+                      "July","August","September","October","November","December"];
+      return months[dateArray[1]-1] + " " + dateArray[2] + ", " + dateArray[0];
+    },
+    // Convert runtime minutes to hours + minutes
+    timeText(minutes) {
+      let hrs = Math.floor(minutes/60);
+      let mins = minutes % 60;
+      return hrs + "h " + mins + "min";
+    },
+    // Change to next poster of current movie
+    posterClick() {
+      let movie = this.currentMovie;
+      movie.posterindex = (movie.posterindex + 1) % movie.posters.length;
+    },
+    // Increment likes for current movie
+    like() { this.currentMovie.likes++; },
+    // Increment dislikes for current movie
+    dislike() { this.currentMovie.dislikes++; },
+    // Show next movie in carousel
+    nextMovie() { this.currentIndex = (this.currentIndex + 1) % this.movies.length; },
+    // Show previous movie in carousel
+    prevMovie() { this.currentIndex = (this.currentIndex - 1 + this.movies.length) % this.movies.length; },
+  }
+});
 
-            }
-      },
-      methods: {
-            makeTextDate(dateArray) {
-                  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-                  let year = dateArray[0];
-                  let month = months[dateArray[1] - 1];
-                  let day = dateArray[2];
-                  return month + " " + day + ", " + year;
-
-            },
-            like(index) {
-                  this.movies[index].likes++
-            },
-            dislike(index) {
-                  this.movies[index].dislikes++
-
-            },
-            posterClick(index) {
-                  let movie = this.movies[index];
-                  movie.posterindex = (movie.posterindex + 1) % movie.posters.length;
-            },
-            timeText(minutes) {
-                  let hrs = Math.floor(minutes / 60);
-                  let mins = minutes % 60;
-                  return hrs + "h " + mins + "min";
-            },
-      }
-})
-
-vue_app.mount("#vue_app")
+vue_app.mount("#vue_app");
